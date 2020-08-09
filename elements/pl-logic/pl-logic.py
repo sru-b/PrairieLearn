dcb51c3f05b56157c5db7cd9ebe3cf00a381fc35
Jack Gronenthal <jlg6@illinois.edu>
2020-08-09 02:03:30 -0500

    Pushing Jack's 2am edits

---
+++ elements/pl-logic/pl-logic.py
@@ -0,0 +1,12 @@
+import prarielearn as pl
+import lxml.html
+
+def prepare(element_html, data):
+    element = lxml.html.fragment_fromstring(element_html)
+    pl.check_attribs(element, required_attribs=[], optional_attribs=[])
+
+def render(element_html, data):
+    element = lxml.html.fragment_fromstring(element_html)
+    test = lxml.html.fragment_fromstring("test")
+    return pl.inner_html(test)
+ 
--- testCourse/elements/logic-element/logic-element.py
+++ testCourse/elements/logic-element/logic-element.py
@@ -1,12 +1,26 @@
 import chevron
+import lxml.html
+import prairielearn as pl
 
 def prepare(element_html, data):
+    element = lxml.html.fragment_fromstring(element_html)
+    optional_attribs = ['logic-ops']
+    pl.check_attribs(element, [], optional_attribs)
+
+    logic_ops = [ { "op": op.strip().lower() } for op in pl.get_string_attrib(element, 'logic-ops').split(",") ]
     data['params']['input_nodes'] = [{"node": "A"}, {"node": "B"}, {"node": "C"}]
+    data['params']['output_nodes'] = [{"node": "X"}]
+    data['params']['logic_ops'] = logic_ops
+
+
     return data
 
 def render(element_html, data):
     html_params = {
-        'input_nodes': data['params']['input_nodes']
+        'input_nodes': data['params']['input_nodes'],
+        'output_nodes': data['params']['output_nodes'],
+        'logic_ops': data['params']['logic_ops'],
+        'image_url': data['options']['client_files_element_url']
     }
     with open('logic-element.mustache', 'r') as f:
         return chevron.render(f, html_params).strip()
